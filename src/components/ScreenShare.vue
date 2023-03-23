148c9ada3be0040b7e49726d73309c74d90bc046
Ryo Ota <nwtgck@nwtgck.org>
2023-03-24 07:43:21 +0900

    key frame request

--- src/components/ScreenShare.vue
+++ src/components/ScreenShare.vue
@@ -1,6 +1,7 @@
 <template>
+  <input v-model="serverUrl" size="60"><br>
+  <input v-model="path" size="60"><br>
   <button @click="share()" style="width: 5rem; height: 5rem;">share</button><br>
-  <br>
   <hr>
   <button @click="view()" style="width: 5rem; height: 5rem;">view</button><br>
   <hr>
@@ -16,8 +17,8 @@ import * as cborX from "cbor-x";
 import {AsyncSemaphore} from "@esfx/async-semaphore";
 
 // const serverUrl = ref("http://localhost:8080/screensharetest/");
-const serverUrl = ref("https://localhost:8443/screensharetest/");
-// const serverUrl = ref("https://ppng.io/screensharetest/");
+// const serverUrl = ref("https://localhost:8443/screensharetest/");
+const serverUrl = ref("https://ppng.io/screensharetest1/");
 const path = ref("path1");
 const myCanvas = ref<HTMLCanvasElement>();
 
@@ -48,7 +49,10 @@ async function share() {
   const videoTrack = mediaStream.getVideoTracks()[0];
   const videoTrackProcessor = new MediaStreamTrackProcessor({ track: videoTrack });
 
+  receiveKeyFrameRequestLoop();
+
   const reader = videoTrackProcessor.readable.getReader();
+  let lastFrame: VideoFrame | undefined;
   while (true) {
     const result = await reader.read();
     if (result.done) {
@@ -63,7 +67,28 @@ async function share() {
       const keyFrame = keyFrameDecider === 0;
       keyFrameDecider = (keyFrameDecider + 1) % 150;
       videoEncoder.encode(frame, { keyFrame });
-      frame.close();
+      lastFrame?.close();
+      lastFrame = frame;
+    }
+  }
+
+  async function receiveKeyFrameRequestLoop() {
+    while (true) {
+      try {
+        const res = await fetch(urlJoin(serverUrl.value, path.value, "keyframe"));
+        if (res.status !== 200) {
+          throw new Error("not OK status");
+        }
+        await res.text();
+        console.log("Encoding key frame...");
+        if (lastFrame !== undefined) {
+          videoEncoder.encode(lastFrame, { keyFrame: true });
+          // Avoid too many key frames
+          keyFrameDecider = 1;
+        }
+      } catch (e) {
+      }
+      await new Promise(resolve => setTimeout(resolve, 2000));
     }
   }
 
@@ -100,7 +125,9 @@ async function view() {
   const pendingVideoFrames: VideoFrame[] = [];
   let underflow = true;
   let baseTime = 0;
-  const downloadSemaphore = new AsyncSemaphore(6);
+  const downloadSemaphore = new AsyncSemaphore(3);
+  let lastHandledTimestamp = -1; // first frame timestamp can be 0
+  let keyFrameRequesting = false;
 
   const videoDecoderConfig: VideoDecoderConfig = {
     codec: "vp8",
@@ -146,6 +173,9 @@ async function view() {
       }
       const chunk = new Uint8Array(await res.arrayBuffer());
       const decoded = cborX.decode(chunk);
+      if (decoded.type === "key") {
+        console.log(decoded.timestamp, "key frame chunk arrived");
+      }
       return {
         timestamp: decoded.timestamp,
         type: decoded.type,
@@ -159,9 +189,38 @@ async function view() {
   }
 
   function handleFrame(frame: VideoFrame) {
+    if (lastHandledTimestamp > frame.timestamp) {
+      requestKeyFrame();
+    }
+    lastHandledTimestamp = frame.timestamp;
     pendingVideoFrames.push(frame);
     if (underflow) {
       setTimeout(renderFrame, 0);
+    }
+  }
+
+  async function requestKeyFrame() {
+    if (keyFrameRequesting) {
+      console.log("already key frame requesting");
+      return;
+    }
+    keyFrameRequesting = true;
+    while (true) {
+      try {
+        const res = await fetch(urlJoin(serverUrl.value, path.value, "keyframe"), {
+          method: "POST",
+        });
+        if (res.status !== 200) {
+          throw new Error("not OK status");
+        }
+        await res.text();
+        console.log("key frame requested");
+        keyFrameRequesting = false;
+        break;
+      } catch (e) {
+        console.warn("failed to request a key frame", e);
+      }
+      await new Promise(resolve => setTimeout(resolve, 2000));
     }
   }
 
